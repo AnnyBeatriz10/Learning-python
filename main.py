31f2737b0d33c2da3d9197a3abe191f6a567dc94
AnnyBeatriz10 <75858073+AnnyBeatriz10@users.noreply.github.com>
2022-01-25 11:34:24 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+n = int (input('Digite um valor:'))
+count = 0
+
+while n != 0:
+  n = int (input('Digite um valor:'))
+  count += 1
+  med =(n + count)//2
+print('a quantidade de números válidos que você digitou foi:',count)
+print('A média dos valores válidos é:',med)
+
+
+  
+  
